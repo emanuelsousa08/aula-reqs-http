173a456700948a4c48b4598d5a38047c5cc7c1bd
Emanuel Sousa <franciscoemanuel23@outlook.com>
2025-04-23 19:12:41 -0300

    atualizações na prática

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-cep.onkeyup = () => {
-    if (cep.value.length == 8) {
-        fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
-        .then(resposta => resposta.json())
-        .then(resposta2 => {
-            rua.value = resposta2.logradouro;
-            bairro.value = resposta2.bairro;
-            cidade.value = resposta2.localidade;
-            estado.value = resposta2.uf;
-        })
-    }
+// JS  da API Viacep
+// cep.onkeyup = () => {
+//     if (cep.value.length == 8) {
+//         fetch(`https://viacep.com.br/ws/${cep.value}/json/`)
+//         .then(resposta => resposta.json())
+//         .then(resposta2 => {
+//             rua.value = resposta2.logradouro;
+//             bairro.value = resposta2.bairro;
+//             cidade.value = resposta2.localidade;
+//             estado.value = resposta2.uf;
+//         })
+//     }
+// }
+
+function buscarRegioes() {
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/regioes")
+    .then((res) => res.json())
+    .then((res) => {
+      res.map((regiao) => {
+        regioes.innerHTML += `<option value="${regiao.id}">${regiao.nome}</option>`;
+      });
+    });
 }
+buscarRegioes();
+
+function buscarEstados() {
+  fetch(
+    `https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regioes.value}/estados`
+  )
+    .then((res) => res.json())
+    .then((res) => {
+      estados.innerHTML = "";
+      res.map((estado) => {
+        estados.innerHTML += `<option value="${estado.id}">${estado.nome}</option>`;
+      });
+    });
+}
+
+// function buscarCidades() {
+//     fetch(
+//       `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estado.id}/municipios`
+//     )
+//       .then((res) => res.json())
+//       .then((res) => {
+//         cidades.innerHTML = "";
+//         res.map((cidade) => {
+//           cidades.innerHTML += `<option value="${cidade.id}">${cidade.nome}</option>`;
+//         });
+//       });
+//   }
